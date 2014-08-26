009f69fa10bd54f7f91c5eb80ee49c1775f41a4b
delta <d@qiscus.com>
2014-08-26 10:56:45 +0700

    update seed for interview

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Product.create!(
-  name: 'Dodecahedron', 
-  price: 2, 
-  description: "Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.",
-  images: [Image.create!(full: 'gem-01.gif', thumb: 'gem-01.gif')],
+  name: 'Lenovo E220s', 
+  price: 650, 
+  description: "The E220s is an affordable and attractive 12-inch business notebook with decent performance but poor battery life.",
+  images: [Image.create!(full: 'http://techreport.com/r.x/2011q1/lenovo-e220s-il.jpg', thumb: 'http://blog.laptopmag.com/wpress/wp-content/uploads/2011/01/Lenovo-ThinkPad-E220S-1.jpg')],
   reviews: [
     Review.create!({
       stars: 5,
@@ -16,19 +16,19 @@ Product.create!(
     })]
 )
 Product.create!(
-  name: 'Pentagonal Gem', 
-  price: 5.95, 
-  description: "Some gems have hidden qualities beyond their luster, beyond their shine... Pentagonal is one of those gems.",
-  images: [Image.create!(full: 'gem-03.gif', thumb: 'gem-03.gif')],
+  name: 'Acer Aspire S7', 
+  price: 595, 
+  description: "The Acer Aspire S7 impresses with a hyper-HD touchscreen and numerous other improvements, making a serious contender for your laptop bag.",
+  images: [Image.create!(full: 'http://img.hexus.net/v2/laptops/Acer/Aspire_S7/S7-04.jpg', thumb: 'http://static.acer.com/up/Resource/Acer/Ultrabooks/AGW2%20Aspire%20S7/Photo%20Gallery/20130416/S7-392-Storm2-photo-gallery-05.png')],
   reviews: [
     Review.create!({
-      stars: 5,
-      body: "I love this product!",
-      author: "joe@thomas.com"
+      stars: 4,
+      body: "Awesome product!",
+      author: "thomas@joe.com"
     }),
     Review.create!({
-      stars: 1,
-      body: "This product sucks",
-      author: "tim@hater.com"
+      stars: 2,
+      body: "Better than nothing",
+      author: "almosthater@tim.com"
     })]
 )
